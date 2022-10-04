bd88d3b029a26404ba82468edd86c1d0c4c67a4a
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-10-04 12:23:34 +0900

    feat: 第２回演習の解答

--- app/02/alu.v
+++ app/02/alu.v
@@ -10,6 +10,7 @@ module alu (
               s==`ALU_THB ? b:
               s==`ALU_AND ? a & b:
               s==`ALU_OR ? a | b:
-              s==`ALU_SL ? a << 1:
-              s==`ALU_SR ? a >> 1:
+              s==`ALU_SL ? ~a: // 論理反転
+              s==`ALU_SR ? a ^ b: //排他的論理和
               s==`ALU_ADD ?  a + b : a-b;
+endmodule
